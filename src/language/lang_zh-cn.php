1cc7ac14d0f1d95cdafa143a79a9a6a4514fac25
zyjs2016 <913540360@qq.com>
2016-12-16 16:40:52 +0800

    Add files via upload

---
+++ src/language/lang_zh-cn.php
@@ -0,0 +1,661 @@
+<?hh // strict
+
+/* HH_IGNORE_ERROR[1002] */
+$translations = array(
+  'date and time format' =>
+    'H:i:s D m/d/Y', //used by date() function
+  //Translations for IndexController
+  'Facebook CTF' =>
+    'Facebook CTF',
+  'Conquer the world' =>
+    '征服世界',
+  'Play' =>
+    '开始',
+  'Welcome to the Facebook Capture the Flag Competition. By clicking "Play," you will be entered into the official CTF challenge. Good luck in your conquest.' =>
+    '欢迎來到 Facebook Capture the Flag 竞赛。立刻点击 "开始" 进入正式的 CTF 挑战。祝征途愉快。',
+  'Get ready for the CTF to start and access the gameboard now!' =>
+    '立即查看游戏板并准备开始进行 CTF！',
+  'Gameboard' =>
+    '游戏板',
+  'Register Team' =>
+    '注册队伍',
+  'Get ready for the CTF to start and register your team now!' =>
+    '立即注册队伍并准备开始进行 CTF！',
+  'Login' =>
+    '登入',
+  'Soon' =>
+    '即將',
+  'Upcoming Game' =>
+    '正在进行',
+  '_days' =>
+    '_天',
+  '_hours' =>
+    '_小时',
+  '_minutes' =>
+    '_分钟',
+  '_seconds' =>
+    '_秒',
+  'Official CTF Rules' =>
+    '正式 CTF 规则',
+  'Following actions are prohibited, unless explicitly told otherwise by event Admins.' =>
+    '除非管理员明确说明，否则禁止以下的行为。',
+  'Rule' =>
+    '规则',
+  'Cooperation' =>
+    '互助',
+  'No cooperation between teams with independent accounts. Sharing of keys or providing revealing hints to other teams is cheating, don’t do it.' =>
+    '队伍之间禁止互助的行为。谨记，分享密钥或是提供提示给其它的队伍将被视为作弊！',
+  'Attacking Scoreboard' =>
+    '攻击记分板',
+  'No attacking the competition infrastructure. If bugs or vulns are found, please alert the competition organizers immediately.' =>
+    '禁止攻击竞赛相关的基础设施。如发现错误或是漏洞，请立即通报管理员。',
+  'Sabotage' =>
+    '妨害',
+  'Absolutely no sabotaging of other competing teams, or in any way hindering their independent progress.' =>
+    '强烈禁止队伍间解题进度之妨害或阻挠。',
+  'Bruteforcing' =>
+    '暴力攻击',
+  'No brute forcing of challenge flag/ keys against the scoring site.' =>
+    '禁止以暴力方式尝试取得记分网站密钥。',
+  'Denial Of Service' =>
+    '阻断服务攻击',
+  'DoSing the CTF platform or any of the challenges is forbidden.' =>
+    '禁止对 CTF 平台或题目实行阻断服务攻击。',
+  'Legal' =>
+    '法律',
+  'Disclaimer' =>
+    '免责声明',
+  'By participating in the contest, you agree to release Facebook and its employees, and the hosting organization from any and all liability, claims or actions of any kind whatsoever for injuries, damages or losses to persons and property which may be sustained in connection with the contest. You acknowledge and agree that Facebook et al is not responsible for technical, hardware or software failures, or other errors or problems which may occur in connection with the contest.' =>
+    '当您参与比赛时，您同意放弃任何及所有对 Facebook 和其员工以及託管组织在竞赛连线期间对人身和财产的任何损失或损害产生的与其相关的任何损害或损失责任的索赔。',
+  'If you have any questions about what is or is not allowed, please ask an organizer.' =>
+    '如果您对上述问题有疑虑，请立即向托管组织反映。',
+  'Have fun!' =>
+    '玩得愉快！',
+  'Name' =>
+    '姓名',
+  'Email' =>
+    '邮箱',
+  'Token' =>
+    'Token',
+  'Team Registration' =>
+    '队伍注册',
+  'Team Name' =>
+    '队伍名称',
+  'Password' =>
+    '密码',
+  'Choose an Emblem' =>
+    '选择一个标志',
+  'Sign Up' =>
+    '注册',
+  'Register to play Capture The Flag here. Once you have registered, you will be logged in.' =>
+    '注册以进行 Capture The Flag 。注册完毕时，会将您自动登入。',
+  'Not Available' =>
+    '服务不可用',
+  'Team Registration will be open soon, stay tuned!' =>
+    '队伍注册即将开放，请静候佳音。',
+  'Try Again' =>
+    '再试一遍',
+  'Select' =>
+    '选择',
+  'Team Login' =>
+    '队伍登入',
+  'Please login here. If you have not registered, you may do so by clicking "Sign Up" below. ' =>
+    '请登入。如果您尚未注册，请点击下方的 "注册" 。',
+  'Team Login will be open soon, stay tuned!' =>
+    '队伍登入即将开放，请静候佳音。',
+  'ERROR' =>
+    '错误',
+  'Start Over' =>
+    '重新来过',
+  'Window is too small' =>
+    '视窗过小',
+  'For the best CTF experience, please make window size bigger.' =>
+    '为了体验最佳的 CTF 游戏，请放大视窗。',
+  'Thank you.' =>
+    '谢谢。',
+  'Logout' =>
+    '登出',
+  'Registration' =>
+    '注册',
+  'Play CTF' =>
+    '进行 CTF',
+  'Rules' =>
+    '规则',
+  //Translations for GameboardController
+  'Admin' =>
+    '后台',
+  'ADMIN' =>
+    '后台',
+  'Navigation' =>
+    '导航',
+  'View Mode' =>
+    '观看模式',
+  'View mode' =>
+    '观看模式',
+  'Tutorial' =>
+    '教学',
+  'Scoreboard' =>
+    '记分板',
+  'You' =>
+    '您',
+  'Others' =>
+    '其他',
+  'All' =>
+    '所有',
+  'Leaderboard' =>
+    '积分榜',
+  'Announcements' =>
+    '公告',
+  'Teams' =>
+    '队伍',
+  'Filter' =>
+    '筛选',
+  'Activity' =>
+    '活动',
+  'Game Clock' =>
+    '游戏时间',
+  //Translations for AdminController
+  'Auto' =>
+    '自动',
+  'All Categories' =>
+    '所有类别',
+  'Open' =>
+    '开放式',
+  'Tokenized' =>
+    'Token式',
+  'Hour' =>
+    '小时',
+  'Hours' =>
+    '小时',
+  'Used by' =>
+    '已使用',
+  'Used By' =>
+    '已使用',
+  'Available' =>
+    '可用的',
+  'Registration Tokens' =>
+    '注册 Tokens',
+  'Create More' =>
+    '建立更多',
+  'Export Available' =>
+    '汇出可用',
+  'Not started yet' =>
+    '尚未开始',
+  'Configuration' =>
+    '设定',
+  'Tokens' =>
+    'Tokens',
+  'Game Configuration' =>
+    '游戏设定',
+  'OK' =>
+    '正常',
+  'status_' =>
+    '状态_',
+  'On' =>
+    '开',
+  'Off' =>
+    '关',
+  'Player Names' =>
+    '玩家名称',
+  'Players Per Team' =>
+    '各队人数',
+  'Registration Type' =>
+    '注册类别',
+  'Strong Passwords' =>
+    '增强式密码',
+  'Team Selection' =>
+    '队伍选择',
+  'Game' =>
+    '游戏',
+  'Scoring' =>
+    '记分板',
+  'Progressive Cycle (s)' =>
+    '计分版更新频率 (秒)',
+  'Refresh Gameboard' =>
+    '记分板重整',
+  'Default Bonus' =>
+    '预设红利',
+  'Bases Cycle (s)' =>
+    'Bases占领计分更新频率 (秒)',
+  'Default Bonus Dec' =>
+    '预设红利损耗',
+  'Timer' =>
+    '计时器',
+  'Server Time' =>
+    '服务器时间',
+  'Game Duration' =>
+    '游戏时间',
+  'Begin Time' =>
+    '开始时间',
+  'Expected End Time' =>
+    '预估结束时间',
+  'Language' =>
+    '语言',
+  'DELETE' =>
+    '删除',
+  'Delete' =>
+    '删除',
+  'No Announcements' =>
+    '无公告',
+  'Game Controls' =>
+    '游戏控制',
+  'Write New Announcement here' =>
+    '在此新增公告',
+  'Create' =>
+    '建立',
+  'General' =>
+    '一般',
+  'Back Up Database' =>
+    '资料库备份',
+  'Export Full Game' =>
+    '汇出游戏进度',
+  'Import Full Game' =>
+    '汇入游戏进度',
+  'Import Teams' =>
+    '汇入队伍资讯',
+  'Export Teams' =>
+    '汇出队伍资讯',
+  'Import Logos' =>
+    '汇入所有标志',
+  'Export Logos' =>
+    '汇出所有标志',
+  'Import Levels' =>
+    '汇入所有关卡',
+  'Export Levels' =>
+    '汇出所有关卡',
+  'Import Categories' =>
+    '汇入所有类别',
+  'Export Categories' =>
+    '汇出所有类别',
+  'Levels' =>
+    '关卡',
+  'New Quiz Level' =>
+    '新增测验型关卡',
+  'Title' =>
+    '标题',
+  'Question' =>
+    '问题',
+  'Level title' =>
+    '关卡标题',
+  'Quiz question' =>
+    '测验问题',
+  'Country' =>
+    '国家',
+  'Answer' =>
+    '答案',
+  'Points' =>
+    '分数',
+  'Hint' =>
+    '提示',
+  'Hint Penalty' =>
+    '提示惩罚',
+  'EDIT' =>
+    '修改',
+  'All Quiz Levels' =>
+    '所有测验型关卡',
+  'Filter By:' =>
+    '过滤选项：',
+  'All Status' =>
+    '所有状态',
+  'Enabled' =>
+    '开启状态',
+  'Disabled' =>
+    '关闭状态',
+  'Quiz Level' =>
+    '测验型关卡',
+  'Show Answer' =>
+    '显示答案',
+  'Bonus' =>
+    '红利',
+  '-Dec' =>
+    '-Dec',
+  'Save' =>
+    '储存',
+  'Quiz Management' =>
+    '测验管理',
+  'Add Quiz Level' =>
+    '增加测验型关卡',
+  'New Flag Level' =>
+    '新增 Flag 型关卡',
+  'Description' =>
+    '叙述',
+  'Level description' =>
+    '关卡叙述',
+  'Category' =>
+    '类别',
+  'Flag' =>
+    'Flag',
+  'flag' =>
+    'Flag',
+  'All Flag Levels' =>
+    '所有Flag型关卡',
+  'New Attachment:' =>
+    '新增附件：',
+  'Attachment' =>
+    '附件',
+  'Link' =>
+    '连结',
+  'New Link:' =>
+    '新增连结：',
+  'Flag Level' =>
+    'Flag关卡',
+  'Categories' =>
+    '所有类别',
+  '+ Attachment' =>
+    '+ 附件',
+  '+ Link' =>
+    '+ 连结',
+  'Flags Management' =>
+    'Flag管理',
+  'Add Flag Level' =>
+    '增加Flag型关卡',
+  'New Base Level' =>
+    '新增Base关卡',
+  'Keep Points' =>
+    '保留得分',
+  'Capture points' =>
+    '夺取得分',
+  'All Base Levels' =>
+    '所有Base关卡',
+  'Base Level' =>
+    'Base关卡',
+  'Bases Management' =>
+    'Base管理',
+  'Add Base Level' =>
+    '增加Base关卡',
+  'New Category' =>
+    '新增类别',
+  'Category: ' =>
+    '类别：',
+  'Categories Management' =>
+    '类别管理',
+  'Add Category' =>
+    '增加类别',
+  'All Countries' =>
+    '所有国家',
+  'In Use' =>
+    '使用中',
+  'In use' =>
+    '使用中',
+  'Not Used' =>
+    '未使用',
+  'Yes' =>
+    '是',
+  'No' =>
+    '否',
+  'ISO Code' =>
+    'ISO 代码',
+  'Countries Management' =>
+    '国家管理',
+  'No Team Names' =>
+    '没有队伍名称',
+  'time' =>
+    '时间',
+  'type' =>
+    '类型',
+  'pts' =>
+    '得分',
+  'Level' =>
+    '关卡',
+  'level' =>
+    '关卡',
+  'No Scores' =>
+    '没有分数',
+  'Attempt' =>
+    '尝试',
+  'No Failures' =>
+    '没有错误',
+  'Team' =>
+    '队伍',
+  'team' =>
+    '队伍',
+  'Names' =>
+    '名称',
+  'Scores' =>
+    '分数',
+  'Failures' =>
+    '错误',
+  'New Team' =>
+    '新增队伍',
+  'Team Logo' =>
+    '队伍标志',
+  'Selected Logo:' =>
+    '已选择的标志:',
+  'Select Logo' =>
+    '选择标志',
+  'All Teams' =>
+    '所有队伍',
+  'Protected' =>
+    '已保护',
+  'Score' =>
+    '分数',
+  'Change Password' =>
+    '密码变更',
+  'Admin Level' =>
+    '管理员等级',
+  'Visibility' =>
+    '队伍可见',
+  'Team Management' =>
+    '队伍管理',
+  'Add Team' =>
+    '增加队伍',
+  'None' =>
+    '无',
+  'Logo Name' =>
+    '标志名称',
+  'Logo Management' =>
+    '标志管理',
+  'Session' =>
+    '回合',
+  'Cookie' =>
+    'Cookie',
+  'Creation Time' =>
+    '建立时间',
+  'Last Access' =>
+    '最后登入',
+  'Last Page Access' =>
+    '最后接触页面',
+  'Data' =>
+    '资讯',
+  'Sessions' =>
+    '回合',
+  'entry' =>
+    '项目',
+  'No Entries' =>
+    '无任何项目',
+  'Game Logs' =>
+    '游戏记录',
+  'Game Logs Timeline' =>
+    '游戏记录时间轴',
+  'End Game' =>
+    '结束游戏',
+  'Begin Game' =>
+    '开始游戏',
+  'Game Admin' =>
+    '游戏管理员',
+  'Controls' =>
+    '控制',
+  'Quiz' =>
+    '测验',
+  'Flags' =>
+    'Flag',
+  'Bases' =>
+    'Base',
+  'Countries' =>
+    '国家',
+  'Logos' =>
+    '标志',
+  //Translations for inc/* and inc/gameboard/*
+  'captured' =>
+    '夺得',
+  'Status' =>
+    '状态',
+  'Completed' =>
+    '已完成',
+  'Remaining' =>
+    '剩馀',
+  'Start' =>
+    '开始',
+  'End' =>
+    '结束',
+  'Rank' =>
+    '排名',
+  'pts' =>
+    '得分', //points
+  'Your Rank' =>
+    '您的排名',
+  'Your Score' =>
+    '您的分数',
+  'Everyone' =>
+    '每个人',
+  'Your Team' =>
+    '您的队伍',
+  'Captured' =>
+    '夺得',
+  'Initiating' =>
+    '初始化',
+  'run : > boot_sequence' =>
+    '进度：> boot_sequence',
+  'Extracting' =>
+    '处理中',
+  //Translations for Utils.php's time_ago() function
+  'just now' =>
+    '刚刚',
+  'd' =>
+    '天', //day
+  'hr' =>
+    '小时', //hour
+  'min' =>
+    '分钟', //minute
+  'sec' =>
+    '秒', //second
+  'ds' =>
+    '天', //days
+  'hrs' =>
+    '小时', //hours
+  'mins' =>
+    '分钟', //minutes
+  'secs' =>
+    '秒', //seconds
+  'ago' =>
+    '前',
+  //Translations for ModalControllers
+  'begin_' =>
+    '开始_',
+  'Are you sure you want to kick off the game? Logs will be cleared and progressive scoreboard will start' =>
+    '您确定要开始游戏了吗？记录将会被清空且记分板将会开始记录。',
+  'end_' =>
+    '结束_',
+  'Are you sure you want to finish the current game?' =>
+    '您确定要立即结束现在的游戏？',
+  'Are you sure you want to logout from the game?' =>
+    '您确定要立即登出？',
+  'Saved' =>
+    '已储存',
+  'All changes have been successfully saved.' =>
+    '所有变更已成功储存。',
+  'Error' =>
+    '错误',
+  'Sorry your form was not saved. Please correct the all errors and save again.' =>
+    '很抱歉我们无法储存您的表单。请修正所有错误并再次尝试。',
+  'cancel_' =>
+    '取消_',
+  'Are you sure you want to cancel? You have unsaved changes that will be reverted.' =>
+    '您确定要取消？未储存的变更将会回复至上次的状态。',
+  'choose_logo' =>
+    '选择标志',
+  'captured_' =>
+    '夺得_',
+  'flag_owner_' =>
+    'Flag_拥有者_',
+  'INACTIVE' =>
+    '未启动',
+  'PTS' =>
+    '分',
+  'category' =>
+    '类别',
+  'capture_' =>
+    '夺取_',
+  'Insert your answer' =>
+    '输入您的答案',
+  'Request Hint' =>
+    '要求提示',
+  'Submit' =>
+    '送出',
+  'hint_' =>
+    '提示_',
+  'first_capture' =>
+    '第一滴血',
+  'completed_by' =>
+    '已完成：',
+  'scoreboard_' =>
+    '记分板_',
+  'filter_' =>
+    '筛选_',
+  'rank_' =>
+    '排名_',
+  'team_name_' =>
+    '队伍名称_',
+  'quiz_pts_' =>
+    '测验型_得分_',
+  'flag_pts_' =>
+    'Flag型_得分_',
+  'base_pts_' =>
+    'Base型_得分_',
+  'total_pts_' =>
+    '总_得分_',
+  'team_' =>
+    '队伍_',
+  'team_members' =>
+    '队伍_成员',
+  'base_pts' =>
+    'Base型_得分',
+  'quiz_pts' =>
+    '测验型_得分',
+  'flag_pts' =>
+    'Flag型_得分_',
+  'total_pts' =>
+    '总_得分',
+  'Tool bars are located on all edges of the gameboard. Tap a category to expand and close each tool bar.' =>
+    '工具列位落于游戏板的四周。点击任一分类以展开或关闭各工具列。',
+  'Tool_Bars' =>
+    '工具_列',
+  'Tap the "Game Clock" to keep track of time during gameplay. Don’t let time get the best of you.' =>
+    '点击 "游戏时间" 以随时追踪剩余时间。别败给时间了。',
+  'Game_Clock' =>
+    '游戏_时间',
+  'Countries marked with an ' =>
+    '有人',
+  'are captured by you.' =>
+    '符号代表您夺得的国家。',
+  ' are owned by others.' =>
+    '符号代表被其他人占有的国家。',
+  'Captures' =>
+    '夺得',
+  'Tap Plus[+] to Zoom In. Tap Minus[-] to Zoom Out.' =>
+    '点击 + [+] 以放大. 点击 - [-] 以缩小。',
+  'Click and Drag to move left, right, up and down.' =>
+    '点击并拖曳以移动版图。',
+  'Zoom' =>
+    '缩放',
+  'Tap Forward Slash [/] to activate computer commands. A list of commands can be found under "Rules".' =>
+    '点击 / [/] 以执行电脑指令。在 "规则" 底下找到可用的指令。',
+  'Command_Line' =>
+    '指令_行',
+  'Click "Nav" to access main navigation links like Rules of Play, Registration, Blog, Jobs & more.' =>
+    '点击 "导航" 以获取更多资讯，例如： 游戏规则、注册、部落格、工作或更多。',
+  'Track your competition by clicking "scoreboard" to access real-time game statistics and graphs.' =>
+    '点击 "记分板" 以追踪您目前游戏的进度，并查看即时的统计资料与图形。',
+  'Have fun, be the best and conquer the world.' =>
+    '成为最棒的征服者吧，祝愉快。',
+  'Game_On' =>
+    '游戏_开始',
+  'tutorial_' =>
+    '教学_',
+  'Next' =>
+    '下一步',
+  'Skip to play' =>
+    '跳过教学',
+  'Powered By Facebook' =>
+    'Powered By Facebook',
+);
